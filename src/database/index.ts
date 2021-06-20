913ae4cbc8cbd370d5f3a69c03366db7b67084b8
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-06-20 23:08:18 +0100

    configurando variaveis hambientes

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
 import { createConnection } from 'typeorm'
-
-createConnection()
+export async function connection (): Promise<void> {
+  await createConnection({
+    type: 'postgres',
+    host: 'localhost',
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    entities: ['./src/modules/users/models/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations'
+    }
+  })
+}
